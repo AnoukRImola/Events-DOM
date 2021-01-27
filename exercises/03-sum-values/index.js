1555c50c14f1b4a48be994ba701086cfc44f5092
Moises Navarro <moi55navacor@gmail.com>
2021-01-27 21:12:22 +0000

    Exercice 3

--- exercises/03-sum-values/index.js
+++ exercises/03-sum-values/index.js
@@ -1,8 +1,10 @@
-window.calculateSumListener = function calculateSumListener()
+window.calculateSumListener = function calculateSumListener() {
 	//Return the value of the input #firstNumber
 	var stringA = document.getElementById("firstNumber").value;
 	//Return the value of the input #secondNumber
 	var stringB = document.getElementById("secondNumber").value;
 
 	//your code goes here
+	var suma = Number(stringA) + Number(stringB); //Se usa la funcion Number(), para pasar un string a Numero
+	document.querySelector("#resultNumber").value = suma; // Se le asigna el valor de suma a la caja resultado con un .value
 };
