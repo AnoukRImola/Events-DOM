8e23f769bfd11d3663130b8609703df7128d88c8
Moises Navarro <moi55navacor@gmail.com>
2021-01-27 21:59:46 +0000

    Exercise 6

--- exercises/06-add-listener-with-js/index.js
+++ exercises/06-add-listener-with-js/index.js
@@ -1,6 +1,14 @@
 window.onload = function myLoadFunction() {
 	alert("The website just finished loading!");
 	//some code here
+	oyente(); //Se llama a la funcion oyente
 };
 
 //the listener function here
+function oyente() {
+	let button = document.querySelector("#theGreen"); //Se optione el valor del elemento
+	button.addEventListener("click", function() {
+		//Se crea una funcion listener, la cual al darle click al boton verde lanzara una alerta
+		alert("wuju");
+	});
+}
