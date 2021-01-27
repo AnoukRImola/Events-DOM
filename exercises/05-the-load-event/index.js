e0a4f9d8d2cf0b7a3f07c83de5c1f7d83b394a87
Moises Navarro <moi55navacor@gmail.com>
2021-01-27 21:36:45 +0000

    Exercise

--- exercises/05-the-load-event/index.js
+++ exercises/05-the-load-event/index.js
@@ -1,1 +1,4 @@
 // your function goes here
+window.loadListener = function loadListener() { //Se crea la funcion loadListener para ser llamada por la etiqueta body cuando la pagina cargue por completo
+	alert("loading finished...");
+};
